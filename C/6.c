e0ceb72ef25f637158bce97074a0ad3c8bf1ae3a
Danis <zinnatullin2012@inbox.ru>
2021-12-11 13:40:37 +0300

    add 7

--- C/6.c
+++ C/6.c
@@ -3,31 +3,22 @@
 #include <limits.h>
 #include <stdio.h>
 //DONE
+void my_print(char * path);
 int main(int argc[], char * argv[], char * envp[]) {
+  char PathName[PATH_MAX];
+    my_print(getcwd(PathName, PATH_MAX));
+    my_print(argv[1]);
+}
+
+void my_print(char * path) {
   DIR * dir;
   struct dirent * ent;
-
-  char PathName[PATH_MAX];
-  if ((dir = opendir(getcwd(PathName, PATH_MAX))) != NULL) {
-    while ((ent = readdir(dir)) != NULL) {
-      printf("%s\n ", ent -> d_name);
-    }
-    closedir(dir);
-  } else {
-
-    perror(" ");
-    return -1;
-
-  }
-
-  if ((dir = opendir(argv[1])) != NULL) {
+  if ((dir = opendir(path)) != NULL) {
     while ((ent = readdir(dir)) != NULL) {
       printf("%s\n ", ent -> d_name);
     }
     closedir(dir);
   } else {
     perror(" ");
-    return -1;
-
   }
 }
